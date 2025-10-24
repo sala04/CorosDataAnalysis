f8cc78da00f2849b7bc365e3a8f9ca54d2b5c8c0
alex <alexsala04@gmail.com>
2025-10-24 16:08:13 +0200

    Path file downloades change to project folder

--- Scraper/driver_manager.py
+++ Scraper/driver_manager.py
@@ -13,6 +13,7 @@ from selenium.webdriver.firefox.service import Service as FirefoxService
 
 def create_drivers (headless=False):
     try:
+
         logging.info("Creating drivers")
         options = Options()
         if headless:
@@ -20,17 +21,16 @@ def create_drivers (headless=False):
             options.add_argument("--window-size=1920,1080")
 
         # ⚙️ Configurar preferencias de descarga
+        FileDir = activity_folder()
+        print(FileDir)
         options.set_preference("browser.download.folderList", 2)  # 2 = ruta personalizada
-        options.set_preference("browser.download.dir", str(activity_folder()))
+        options.set_preference("browser.download.dir", FileDir) #str(activity_folder())
         options.set_preference("browser.download.manager.showWhenStarting", False)
-        options.set_preference(
-            "browser.helperApps.neverAsk.saveToDisk",
-            "application/zip,application/gpx+xml,text/csv,application/octet-stream,application/vnd.google-earth.kml+xml"
-        )
+        options.set_preference("browser.helperApps.neverAsk.saveToDisk","application/octet-stream,application/pdf")
         options.set_preference("pdfjs.disabled", True)  # Desactiva visor de PDF integrado
 
         driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()), options=options)
-
+        
         return driver
 
     except:
@@ -41,7 +41,8 @@ def activity_folder():
     timestamp = datetime.now().strftime("%d_%m_%Y_%H-%M-%S")
     BASE_DIR = os.path.dirname(os.path.abspath(__file__))
     ROOT_DIR = os.path.dirname(BASE_DIR)
-    LOG_DIR = os.path.join(ROOT_DIR, f"Activities/{timestamp}")
+    LOG_DIR = os.path.join(ROOT_DIR, f"Activities\\{timestamp}")
     os.makedirs(LOG_DIR, exist_ok=True)
 
     return LOG_DIR
+
